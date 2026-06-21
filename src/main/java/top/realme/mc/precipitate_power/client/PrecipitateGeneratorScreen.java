73cd2042182ffe9305868661ead48d10a991b3c3
harmog <harmog@foxmail.com>
2026-06-21 23:19:54 +0800

    feat: 使用LDLib2构建UI

--- src/main/java/top/realme/mc/precipitate_power/client/PrecipitateGeneratorScreen.java
+++ src/main/java/top/realme/mc/precipitate_power/client/PrecipitateGeneratorScreen.java
@@ -3,9 +3,7 @@ package top.realme.mc.precipitate_power.client;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
-import org.joml.Math;
 import top.realme.mc.precipitate_power.menu.PrecipitateGeneratorMenu;
 
 public class PrecipitateGeneratorScreen extends AbstractContainerScreen<PrecipitateGeneratorMenu> {
@@ -18,45 +16,9 @@ public class PrecipitateGeneratorScreen extends AbstractContainerScreen<Precipit
 
     @Override
     protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
-        int x = leftPos;
-        int y = topPos;
-        guiGraphics.fill(x, y, x + imageWidth, y + imageHeight, 0xFF1E2026);
-        guiGraphics.fill(x + 6, y + 17, x + 170, y + 71, 0xFF2A2D36);
-        guiGraphics.fill(x + 43, y + 34, x + 61, y + 52, 0xFF4B5160);
-        guiGraphics.fill(x + 115, y + 34, x + 133, y + 52, 0xFF4B5160);
-        guiGraphics.fill(x + 80, y + 18, x + 96, y + 70, 0xFF16181D);
-        if (menu.getMaxWaterStored() > 0) {
-            guiGraphics.fill(x + 62, y + 18, x + 78, y + 70, 0xFF16181D);
-        }
-
-        int energy = menu.getEnergyStored();
-        int capacity = Math.max(1, menu.getMaxEnergyStored());
-        int height = Mth.clamp((int) ((energy / (float) capacity) * 50), 0, 50);
-        guiGraphics.fill(x + 82, y + 68 - height, x + 94, y + 68, 0xFF46C266);
-
-        if (menu.getMaxWaterStored() > 0) {
-            int waterHeight = Mth.clamp((int) ((menu.getWaterStored() / (float) menu.getMaxWaterStored()) * 50), 0, 50);
-            guiGraphics.fill(x + 64, y + 68 - waterHeight, x + 76, y + 68, 0xFF3B82F6);
-        }
     }
 
     @Override
     protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
-        //guiGraphics.drawString(font, title, titleLabelX, titleLabelY, 0xF0F0F0, false);
-        guiGraphics.drawString(font, playerInventoryTitle, inventoryLabelX, inventoryLabelY, 0xC7CBD6, false);
-        guiGraphics.drawString(font, Component.translatable("gui.precipitate_power.energy", menu.getEnergyStored(), menu.getMaxEnergyStored()), 8, 6, 0x8ED0A7, false);
-        guiGraphics.drawString(font, Component.translatable("gui.precipitate_power.max_extract", menu.getMaxExtract(), menu.getExtraMaxExtract()), 8, 18, 0xE7C166, false);
-        if (menu.getMaxWaterStored() > 0) {
-            guiGraphics.drawString(font, Component.translatable("gui.precipitate_power.water", menu.getWaterStored(), menu.getMaxWaterStored()), 8, 30, 0x7DB7FF, false);
-        }
-//        guiGraphics.drawString(font, Component.translatable("gui.precipitate_power.precipitation", menu.getPrecipitationLevel()), 8, 18, 0x8AC7FF, false);
-//        guiGraphics.drawString(font, Component.translatable("gui.precipitate_power.dirty_count", menu.getDirtyCount()), 8, 30, 0xD7DCE6, false);
-    }
-
-    @Override
-    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        renderBackground(guiGraphics, mouseX, mouseY, partialTick);
-        super.render(guiGraphics, mouseX, mouseY, partialTick);
-        renderTooltip(guiGraphics, mouseX, mouseY);
     }
 }
--- src/main/java/top/realme/mc/precipitate_power/menu/PrecipitateGeneratorMenu.java
+++ src/main/java/top/realme/mc/precipitate_power/menu/PrecipitateGeneratorMenu.java
@@ -1,6 +1,16 @@
 package top.realme.mc.precipitate_power.menu;
 
+import com.lowdragmc.lowdraglib2.gui.holder.IModularUIHolderMenu;
+import com.lowdragmc.lowdraglib2.gui.sync.bindings.impl.SupplierDataSource;
+import com.lowdragmc.lowdraglib2.gui.texture.ColorRectTexture;
+import com.lowdragmc.lowdraglib2.gui.ui.ModularUI;
+import com.lowdragmc.lowdraglib2.gui.ui.UI;
+import com.lowdragmc.lowdraglib2.gui.ui.UIElement;
+import com.lowdragmc.lowdraglib2.gui.ui.elements.Label;
+import com.lowdragmc.lowdraglib2.gui.ui.elements.ProgressBar;
+import com.lowdragmc.lowdraglib2.gui.ui.style.StylesheetManager;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
@@ -10,6 +20,7 @@ import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import org.appliedenergistics.yoga.YogaPositionType;
 import top.realme.mc.precipitate_power.registry.ModMenus;
 import top.realme.mc.precipitate_power.util.SockDataUtil;
 
@@ -42,6 +53,69 @@ public class PrecipitateGeneratorMenu extends AbstractContainerMenu {
         addPlayerInventory(inventory);
         addPlayerHotbar(inventory);
         addDataSlots(data);
+
+        if (this instanceof IModularUIHolderMenu holder) {
+            holder.setModularUI(createModularUI(inventory.player));
+        }
+    }
+
+    private ModularUI createModularUI(Player player) {
+        UIElement root = new UIElement();
+        root.layout(layout -> layout.width(176).height(166));
+        root.style(style -> style.background(new ColorRectTexture(0xFF1E2026)));
+
+        root.addChildren(
+                box(6, 17, 164, 54, 0xFF2A2D36),
+                box(43, 34, 18, 18, 0xFF4B5160),
+                box(115, 34, 18, 18, 0xFF4B5160),
+                box(62, 18, 16, 52, 0xFF16181D),
+                box(80, 18, 16, 52, 0xFF16181D),
+                dynamicLabel(8, 6, () -> Component.translatable("gui.precipitate_power.energy", getEnergyStored(), getMaxEnergyStored())),
+                dynamicLabel(8, 18, () -> Component.translatable("gui.precipitate_power.max_extract", getMaxExtract(), getExtraMaxExtract())),
+                dynamicLabel(8, 30, () -> getMaxWaterStored() > 0
+                        ? Component.translatable("gui.precipitate_power.water", getWaterStored(), getMaxWaterStored())
+                        : Component.empty()),
+                dynamicLabel(8, 42, () -> Component.translatable("gui.precipitate_power.precipitation", getPrecipitationLevel())),
+                dynamicLabel(8, 54, () -> Component.translatable("gui.precipitate_power.dirty_count", getDirtyCount())),
+                progressBar(82, 18, 12, 50, 0xFF46C266,
+                        () -> getMaxEnergyStored() <= 0 ? 0.0F : getEnergyStored() / (float) getMaxEnergyStored(),
+                        () -> Component.translatable("gui.precipitate_power.energy", getEnergyStored(), getMaxEnergyStored())),
+                progressBar(64, 18, 12, 50, 0xFF3B82F6,
+                        () -> getMaxWaterStored() <= 0 ? 0.0F : getWaterStored() / (float) getMaxWaterStored(),
+                        () -> Component.translatable("gui.precipitate_power.water", getWaterStored(), getMaxWaterStored()))
+        );
+
+        return ModularUI.of(
+                UI.of(root, StylesheetManager.INSTANCE.getStylesheetSafe(StylesheetManager.GDP)),
+                player
+        );
+    }
+
+    private static UIElement box(int left, int top, int width, int height, int color) {
+        UIElement element = new UIElement();
+        element.layout(layout -> layout.positionType(YogaPositionType.ABSOLUTE).left(left).top(top).width(width).height(height));
+        element.style(style -> style.background(new ColorRectTexture(color)));
+        return element;
+    }
+
+    private static Label dynamicLabel(int left, int top, java.util.function.Supplier<Component> supplier) {
+        Label label = new Label();
+        label.layout(layout -> layout.positionType(YogaPositionType.ABSOLUTE).left(left).top(top));
+        label.bindDataSource(SupplierDataSource.of(supplier).frequency(1));
+        return label;
+    }
+
+    private static ProgressBar progressBar(int left, int top, int width, int height, int fillColor,
+                                           java.util.function.Supplier<Float> progressSupplier,
+                                           java.util.function.Supplier<Component> tooltipSupplier) {
+        ProgressBar bar = new ProgressBar();
+        bar.layout(layout -> layout.positionType(YogaPositionType.ABSOLUTE).left(left).top(top).width(width).height(height));
+        bar.setMinValue(0.0F);
+        bar.setMaxValue(1.0F);
+        bar.bindDataSource(SupplierDataSource.of(progressSupplier).frequency(1));
+        bar.barContainer(container -> container.style(style -> style.background(new ColorRectTexture(0xFF16181D))));
+        bar.bar(inner -> inner.style(style -> style.background(new ColorRectTexture(fillColor)).tooltips(tooltipSupplier.get())));
+        return bar;
     }
 
     public int getEnergyStored() {
